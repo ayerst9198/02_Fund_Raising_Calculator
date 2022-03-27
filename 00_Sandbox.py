43b165e6a457a41860f10d6d1b9116ebdfc3f13f
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2022-03-28 11:35:03 +1300

    Update 00_Sandbox.py

---
+++ 00_Sandbox.py
@@ -0,0 +1,15 @@
+from turtle import *
+import time
+
+bgcolor("blue")
+color("red")
+begin_fill()
+pensize(10)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+time.sleep(10)
