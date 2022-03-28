b8d6f6b55c5b74c06fee0ec2c46170c398878a67
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2022-03-29 10:59:58 +1300

    update

--- 01_Base_Component_V1.py
+++ 01_Base_Component_V1.py
@@ -1,6 +1,6 @@
 import pandas
 import time
-import random
+from turtle import Turtle
 
 def num_check(question, num_type, error, low=None, high=None, exit_code=None):
 
@@ -145,18 +145,119 @@ def expense_print(heading, frame, subtotal):
     print("{} Costs: ${:.2f}".format(heading, subtotal))
     return ""
 
+# kirbo
 def kirbo():
     count = 0
   
-    while 1 == 1:
-        num = random.randint(1,10)
-        count += 1
-        if num == 1:
-            print("demon #{}".format(count))
-        else:
-            print("kirbo #{}".format(count))
+    while count != 10000:
+        count += 8
+        print("kirbo #{}".format(count))
+    myTurtle = Turtle()
+    myTurtle.hideturtle()
+    myTurtle.setpos(-200,200)
+    myTurtle.pos()
+    side = 20
+    #color pixel method
+    def square(color):
+        myTurtle.speed(5000)
+        myTurtle.color(color)
+        myTurtle.begin_fill()
+        for i in range(4):
+            myTurtle.forward(side)
+            myTurtle.right(90)
+        myTurtle.end_fill()
+        myTurtle.forward(side)
+        
+    #next row method, doing rows from top to bottom
+    def nextRow(numSquares):
+        myTurtle.speed(5000)
+        myTurtle.penup()
+        myTurtle.back(numSquares * side)
+        myTurtle.right(90)
+        myTurtle.forward(side)
+        myTurtle.left(90)
+        myTurtle.pendown()
+        
+    # prints the row of pixels that color
+    def row(colors,numbers):
+        myTurtle.speed(5000)
+        numsquares = 0;
+        for i in range(len(colors)):
+            for i2 in range(numbers[i]):
+                square(colors[i])
+            numsquares += numbers[i]
+        nextRow(numsquares) # reset the cursor
 
 
+    myTurtle.speed(5000)   
+    # colors we have
+    # colors = ["white" ,"black", "pink", "deeppink"]
+    pink = "#FF99CF"
+    darkpink = "#B54365"
+    # row 1
+    colors = ["white", "black", "white", "black", "white"]
+    numbers = [ 2, 2, 1, 5, 6]
+    row(colors,numbers)
+    # row 2
+    colors = ["white", "black", pink, "black", pink, "black", "white"]
+    numbers = [ 1, 1, 2, 1, 5, 2, 4]
+    row(colors,numbers)
+    # row 3 bppbppppppppbwww
+    colors = ["black", pink, "black", pink, "black", "white"]
+    numbers = [ 1, 2, 1, 8, 1, 3]
+    row(colors,numbers)
+    # row 4 bpppppbpbppppbww
+    for r in range(2): # row 4 and 5 are the same
+        colors = ["black", pink, "black", pink, "black", pink, "black", "white"]
+        numbers = [ 1, 5, 1, 1, 1, 4, 1, 2]
+        row(colors,numbers)
+    # row 6 bpppppbpbpppppbw
+    colors = ["black", pink, "black", pink, "black", pink, "black", "white"]
+    numbers = [ 1, 5, 1, 1, 1, 5, 1, 1]
+    row(colors,numbers)
+    # row 7 bpppddpppddppppb
+    colors = ["black", pink, darkpink, pink, darkpink, pink, "black"]
+    numbers = [ 1, 3, 2, 3, 2, 4, 1]
+    row(colors,numbers)
+    # row 8 bppppppbpppppppb
+    colors = ["black", pink, "black", pink, "black"]
+    numbers = [ 1, 6, 1, 7, 1]
+    row(colors,numbers)
+    # row 9 wbpppppbpppppppb
+    colors = ["white", "black", pink, "black", pink, "black"]
+    numbers = [ 1, 1, 5, 1, 7, 1]
+    row(colors,numbers)
+    # row 10 wbppppppppppbbbw
+    colors = ["white", "black", pink, "black", "white"]
+    numbers = [ 1, 1, 10, 3, 1]
+    row(colors,numbers)
+    # row 11 wbpppppppppbdddb
+    colors = ["white", "black", pink, "black", darkpink, "black"]
+    numbers = [ 1, 1,9, 1, 3, 1]
+    row(colors,numbers)
+    # row 12 wwbpppppppbddddb
+    colors = ["white", "black", pink, "black", darkpink, "black"]
+    numbers = [ 2, 1, 7, 1, 4, 1]
+    row(colors,numbers)
+    # row 13  wwbbppppppbddddb
+    colors = ["white", "black", pink, "black", darkpink, "black"]
+    numbers = [ 2, 2, 6, 1, 4, 1]
+    row(colors,numbers)
+    # row 14 wbddbbpppbddddbw
+    colors = ["white", "black", darkpink, "black", pink, "black", darkpink, "black", "white"]
+    numbers = [ 1, 1, 2, 2, 3, 1, 4, 1, 1]
+    row(colors,numbers)
+    # row 15 bdddddbbbbbddbww
+    colors = ["black", darkpink, "black", darkpink, "black", "white"]
+    numbers = [ 1, 5, 5, 2, 1, 2]
+    row(colors,numbers)
+    # row 16 wbbbbbbwwwbbbwww
+    colors = ["white", "black", "white", "black", "white"]
+    numbers = [ 1, 6, 3, 3, 3]
+    row(colors,numbers)
+    time.sleep(10)
+
+# upupdowndownleftrightleftrightbastart
 def konami():
 
     print()
