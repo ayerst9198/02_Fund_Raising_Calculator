f7ee6e14c700e6756fa42f7a0efc37a3b71b3e80
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2022-03-30 09:05:22 +1300

    Update 00_Sandbox.py

--- 00_Sandbox.py
+++ 00_Sandbox.py
@@ -37,72 +37,66 @@ def row(colors,numbers):
 myTurtle.speed(500)   
 # colors we have
 # colors = ["white" ,"black", "pink", "deeppink"]
-def kirby():
-
-    pink = "#FF99CF"
-    darkpink = "#B54365"
-    # row 1
-    colors = ["white", "black", "white", "black", "white"]
-    numbers = [ 2, 2, 1, 5, 6]
-    row(colors,numbers)
-    # row 2
-    colors = ["white", "black", pink, "black", pink, "black", "white"]
-    numbers = [ 1, 1, 2, 1, 5, 2, 4]
-    row(colors,numbers)
-    # row 3 bppbppppppppbwww
-    colors = ["black", pink, "black", pink, "black", "white"]
-    numbers = [ 1, 2, 1, 8, 1, 3]
-    row(colors,numbers)
-    # row 4 bpppppbpbppppbww
-    for r in range(2): # row 4 and 5 are the same
-        colors = ["black", pink, "black", pink, "black", pink, "black", "white"]
-        numbers = [ 1, 5, 1, 1, 1, 4, 1, 2]
-        row(colors,numbers)
-    # row 6 bpppppbpbpppppbw
+pink = "#FF99CF"
+darkpink = "#B54365"
+# row 1
+colors = ["white", "black", "white", "black", "white"]
+numbers = [ 2, 2, 1, 5, 6]
+row(colors,numbers)
+# row 2
+colors = ["white", "black", pink, "black", pink, "black", "white"]
+numbers = [ 1, 1, 2, 1, 5, 2, 4]
+row(colors,numbers)
+# row 3 bppbppppppppbwww
+colors = ["black", pink, "black", pink, "black", "white"]
+numbers = [ 1, 2, 1, 8, 1, 3]
+row(colors,numbers)
+# row 4 bpppppbpbppppbww
+for r in range(2): # row 4 and 5 are the same
     colors = ["black", pink, "black", pink, "black", pink, "black", "white"]
-    numbers = [ 1, 5, 1, 1, 1, 5, 1, 1]
+    numbers = [ 1, 5, 1, 1, 1, 4, 1, 2]
     row(colors,numbers)
-    # row 7 bpppddpppddppppb
-    colors = ["black", pink, darkpink, pink, darkpink, pink, "black"]
-    numbers = [ 1, 3, 2, 3, 2, 4, 1]
-    row(colors,numbers)
-    # row 8 bppppppbpppppppb
-    colors = ["black", pink, "black", pink, "black"]
-    numbers = [ 1, 6, 1, 7, 1]
-    row(colors,numbers)
-    # row 9 wbpppppbpppppppb
-    colors = ["white", "black", pink, "black", pink, "black"]
-    numbers = [ 1, 1, 5, 1, 7, 1]
-    row(colors,numbers)
-    # row 10 wbppppppppppbbbw
-    colors = ["white", "black", pink, "black", "white"]
-    numbers = [ 1, 1, 10, 3, 1]
-    row(colors,numbers)
-    # row 11 wbpppppppppbdddb
-    colors = ["white", "black", pink, "black", darkpink, "black"]
-    numbers = [ 1, 1,9, 1, 3, 1]
-    row(colors,numbers)
-    # row 12 wwbpppppppbddddb
-    colors = ["white", "black", pink, "black", darkpink, "black"]
-    numbers = [ 2, 1, 7, 1, 4, 1]
-    row(colors,numbers)
-    # row 13  wwbbppppppbddddb
-    colors = ["white", "black", pink, "black", darkpink, "black"]
-    numbers = [ 2, 2, 6, 1, 4, 1]
-    row(colors,numbers)
-    # row 14 wbddbbpppbddddbw
-    colors = ["white", "black", darkpink, "black", pink, "black", darkpink, "black", "white"]
-    numbers = [ 1, 1, 2, 2, 3, 1, 4, 1, 1]
-    row(colors,numbers)
-    # row 15 bdddddbbbbbddbww
-    colors = ["black", darkpink, "black", darkpink, "black", "white"]
-    numbers = [ 1, 5, 5, 2, 1, 2]
-    row(colors,numbers)
-    # row 16 wbbbbbbwwwbbbwww
-    colors = ["white", "black", "white", "black", "white"]
-    numbers = [ 1, 6, 3, 3, 3]
-    row(colors,numbers)
-
-
-a = input()
-kirby()
+# row 6 bpppppbpbpppppbw
+colors = ["black", pink, "black", pink, "black", pink, "black", "white"]
+numbers = [ 1, 5, 1, 1, 1, 5, 1, 1]
+row(colors,numbers)
+# row 7 bpppddpppddppppb
+colors = ["black", pink, darkpink, pink, darkpink, pink, "black"]
+numbers = [ 1, 3, 2, 3, 2, 4, 1]
+row(colors,numbers)
+# row 8 bppppppbpppppppb
+colors = ["black", pink, "black", pink, "black"]
+numbers = [ 1, 6, 1, 7, 1]
+row(colors,numbers)
+# row 9 wbpppppbpppppppb
+colors = ["white", "black", pink, "black", pink, "black"]
+numbers = [ 1, 1, 5, 1, 7, 1]
+row(colors,numbers)
+# row 10 wbppppppppppbbbw
+colors = ["white", "black", pink, "black", "white"]
+numbers = [ 1, 1, 10, 3, 1]
+row(colors,numbers)
+# row 11 wbpppppppppbdddb
+colors = ["white", "black", pink, "black", darkpink, "black"]
+numbers = [ 1, 1,9, 1, 3, 1]
+row(colors,numbers)
+# row 12 wwbpppppppbddddb
+colors = ["white", "black", pink, "black", darkpink, "black"]
+numbers = [ 2, 1, 7, 1, 4, 1]
+row(colors,numbers)
+# row 13  wwbbppppppbddddb
+colors = ["white", "black", pink, "black", darkpink, "black"]
+numbers = [ 2, 2, 6, 1, 4, 1]
+row(colors,numbers)
+# row 14 wbddbbpppbddddbw
+colors = ["white", "black", darkpink, "black", pink, "black", darkpink, "black", "white"]
+numbers = [ 1, 1, 2, 2, 3, 1, 4, 1, 1]
+row(colors,numbers)
+# row 15 bdddddbbbbbddbww
+colors = ["black", darkpink, "black", darkpink, "black", "white"]
+numbers = [ 1, 5, 5, 2, 1, 2]
+row(colors,numbers)
+# row 16 wbbbbbbwwwbbbwww
+colors = ["white", "black", "white", "black", "white"]
+numbers = [ 1, 6, 3, 3, 3]
+row(colors,numbers)
